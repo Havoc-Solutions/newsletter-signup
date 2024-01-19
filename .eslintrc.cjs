881f1eba40c707fd3847afc16ca778df5efd47cd
EONRaider <agsilva@tutanota.com>
2024-01-18 21:05:48 -0300

    Add TypeScript linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,4 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
-  // root: true,
 }
